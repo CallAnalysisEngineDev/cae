3555635169db7d1a73c56b9c3aa5619994d14338
xiongjunlang <xiongjunlang@agree.com.cn>
2017-06-06 17:04:35 +0800

    log4j文件路径动态化

--- src/org/cae/aspect/CallAspect.java
+++ src/org/cae/aspect/CallAspect.java
@@ -4,7 +4,6 @@ import org.apache.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.cae.dao.IBaseDao;
 
 @Aspect
